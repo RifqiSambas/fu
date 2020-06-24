247d19af84c79c09fe3c190447ea1e988b82f080
rifqisambas <rifqisambas@tuta.io>
2020-06-24 11:16:39 +0700

    fix slider

--- application/views/home/component/statistik.php
+++ application/views/home/component/statistik.php
@@ -1,11 +1,11 @@
 <h4 class="p-title"><b>STATISTIK PENGUNJUNG</b></h4>
 <div class="row mb-20">
-	<div class="col-lg-2">
+	<div class="col-lg-4">
 		<div class="letter-big plr-5 bg-primary color-white bg-primary">
 			<?= $pengunjung['today'] ?>
 		</div>
 	</div>
-	<div class="col-lg-10 mt-20">
+	<div class="col-lg-8 mt-20">
 		<h5>
 			<b>
 				PENGUNJUNG HARI INI
@@ -14,12 +14,12 @@
 	</div>
 </div>
 <div class="row mb-20">
-	<div class="col-lg-2">
+	<div class="col-lg-4">
 		<div class="letter-big plr-5 bg-primary color-white bg-primary">
 			<?= $pengunjung['total'] ?>
 		</div>
 	</div>
-	<div class="col-lg-10 mt-20">
+	<div class="col-lg-8 mt-20">
 		<h5>
 			<b>
 				TOTAL PENGUNJUNG
@@ -28,12 +28,12 @@
 	</div>
 </div>
 <div class="row">
-	<div class="col-lg-2">
+	<div class="col-lg-4">
 		<div class="letter-big plr-5 bg-primary color-white bg-primary">
 			<?= $pengunjung['online'] ?>
 		</div>
 	</div>
-	<div class="col-lg-10 mt-20">
+	<div class="col-lg-8 mt-20">
 		<h5>
 			<b>
 				PENGUNJUNG ONLINE
